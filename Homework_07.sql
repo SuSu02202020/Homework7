6a05e4036b79a0469ccae51d09951e1ade6f8c37
Gata Kass <sueyahara@yahoo.com>
2018-10-02 11:10:46 -0400

    till 4a

--- Homework_07.sql
+++ Homework_07.sql
@@ -44,3 +44,31 @@ add column description blob after last_name;
 Delete the `description` column.*/
 alter table actor
 drop description;
+
+/*4a. List the last names of actors, as well as how many actors have that last name.*/
+select last_name, count(last_name) as same_last_name
+from actor
+group by last_name;
+
+/*4b. List last names of actors and the number of actors who have that last name, but 
+only for names that are shared by at least two actors*/
+select last_name, count(last_name) as at_least_two
+from actor
+group by last_name
+having count(last_name) >= 2;
+
+/*4c. The actor `HARPO WILLIAMS` was accidentally entered in the `actor` table as 
+`GROUCHO WILLIAMS`. Write a query to fix the record.*/
+update actor
+set first_name = 'HARPO' 
+where first_name = 'GROUCHO' and last_name = 'Williams';
+
+/*4d. Perhaps we were too hasty in changing `GROUCHO` to `HARPO`. It turns out that 
+`GROUCHO` was the correct name after all! In a single query, if the first name of the actor
+ is currently `HARPO`, change it to `GROUCHO`.*/
+update actor
+set first_name = 'GROUCHO' 
+where first_name = 'HARPO' and last_name = 'Williams';
+
+/*5a. You cannot locate the schema of the `address` table. Which query would you use to re-create it?*/
+
